e6d52ce2a754387af541cfc754236da079f76ef2
Brad Flaugher <brad@bradflaugher.com>
2023-02-06 13:54:49 +0000

    better deploy scripts

--- html/deploy_prod.sh
+++ html/deploy_prod.sh
@@ -5,7 +5,15 @@
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html 
 # aws configure
 
-aws s3 cp dist s3://bradflaugher.com --recursive
+# you need two environment variables, if you don't know what those are just use this search
+# something like
+# export MYSITE_S3_BUCKET = 's3://example.com'
+# export MYSITE_CLOUNDFRONT_DIST = 'AOEUAOEUAOUE'
+# for more info, look around duckduckgo
+# https://duckduckgo.com/?q=%22setup%22+environment+variables+%22stack%22+%22overflow%22&norw=1&ia=web
+
+aws s3 cp dist $MYSITE_S3_BUCKET --recursive
 aws cloudfront create-invalidation \
-    --distribution-id E2EGK5A9TUYOW1 \
+    --distribution-id $MYSITE_CLOUNDFRONT_DIST \
     --paths "/*"
+
