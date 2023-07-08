c6a70ea99807236f9b8becbf4bd693063462de83
Brad Flaugher <brad@bradflaugher.com>
2023-07-08 19:52:41 +0000

    updated node environment

--- html/gulpfile.mjs
+++ html/gulpfile.mjs
@@ -1,3 +1,28 @@
+import gulp from 'gulp';
+const { src, dest, series, parallel } = gulp;
+import browserSync from 'browser-sync';
+import fileInclude from 'gulp-file-include';
+import { deleteAsync } from 'del';
+import sass from 'gulp-sass';
+import * as nodeSass from 'sass'
+import autoprefixer from 'gulp-autoprefixer';
+import group_media from 'gulp-group-css-media-queries';
+import clean_css from 'gulp-clean-css';
+import rename from 'gulp-rename';
+import uglifyEs from 'gulp-uglify-es';
+const uglify = uglifyEs.default;
+import notify from 'gulp-notify';
+import plumber from 'gulp-plumber';
+import panini from 'panini';
+import babel from 'gulp-babel';
+import imagemin from 'gulp-imagemin';
+import gifsicle from 'imagemin-gifsicle';
+import mozjpeg from 'imagemin-mozjpeg';
+import optipng from 'imagemin-optipng';
+import svgo from 'imagemin-svgo';
+import htmlMin from 'gulp-htmlmin';
+import ttf2woff2 from 'gulp-ttf2woff2';
+
 const distPath = 'dist/'
 const srcPath = 'src/'
 
@@ -24,36 +49,10 @@ const path = {
         img: srcPath + 'assets/img/**/*.{jpg,png,svg,gif,ico,webp}'
     },
 
-    clean: './' + distPath + '/',
+    clean: './' + distPath + '/**/*',
 }
 
-const {
-    src,
-    dest,
-    series,
-    parallel
-} = require('gulp')
-const gulp = require('gulp')
-
-
-const browserSync = require('browser-sync')
-
-const fileInclude = require('gulp-file-include')
-const del = require('del')
-const scss = require('gulp-sass')(require('sass')); 
-const autoprefixer = require('gulp-autoprefixer')
-const group_media = require('gulp-group-css-media-queries')
-const clean_css = require('gulp-clean-css')
-const rename = require('gulp-rename')
-const uglify = require('gulp-uglify-es').default
-const notify = require('gulp-notify')
-const plumber = require('gulp-plumber')
-const panini = require('panini')
-const babel = require('gulp-babel')
-const imageMin = require('gulp-imagemin')
-const htmlMin = require('gulp-htmlmin');
-const ttf2woff = require('gulp-ttf2woff')
-const ttf2woff2 = require('gulp-ttf2woff2')
+const scss = sass(nodeSass); 
 
 function browserSyncF() {
     browserSync.init({
@@ -163,21 +162,11 @@ function images() {
         .pipe(dest(path.build.img))
         .pipe(src(path.src.img))
         .pipe(
-            imageMin([
-                imageMin.gifsicle(
-                    { interlaced: true }
-                ),
-                imageMin.mozjpeg(
-                    { quality: 100, progressive: true }
-                ),
-                imageMin.optipng(
-                    { optimizationLevel: 3 }
-                ),
-                imageMin.svgo({
-                    plugins: [
-                        { removeViewBox: true }, { cleanupIDs: false }
-                    ],
-                }),
+            imagemin([
+                gifsicle({ interlaced: true }),
+                mozjpeg({ quality: 100, progressive: true }),
+                optipng({ optimizationLevel: 3 }),
+                svgo(),
             ])
         )
         .pipe(dest(path.build.img))
@@ -185,10 +174,6 @@ function images() {
 }
 
 function fonts() {
-    src(path.src.fonts)
-        .pipe(ttf2woff())
-        .pipe(dest(path.build.fonts))
-
     return src(path.src.fonts)
         .pipe(ttf2woff2())
         .pipe(dest(path.build.fonts))
@@ -211,7 +196,7 @@ function watchFiles(cb) {
 }
 
 function clean() {
-    return del(path.clean)
+    return deleteAsync(path.clean)
 }
 
 let build = series(
@@ -243,6 +228,5 @@ let watch = series(
     browserSyncF
 )
 
-exports.default = watch
-exports.build = build
-exports.build_prod = build_prod
+export default watch
+export { build, build_prod }
